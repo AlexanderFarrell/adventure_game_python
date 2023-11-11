cb29edac1dfd54a4fc848fe634f379ef1aacb413
alexander-farrell <irishsnowday@icloud.com>
2023-11-11 12:17:26 -0500

    Added maps to the game.

---
+++ map.py
@@ -0,0 +1,36 @@
+import pygame
+
+class TileKind:
+    def __init__(self, name, image, is_solid):
+        self.name = name
+        self.image = pygame.image.load(image)
+        self.is_solid = is_solid
+
+class Map:
+    def __init__(self, map_file, tile_kinds, tile_size):
+        # Load the data from the file
+        file = open(map_file, "r")
+        data = file.read()
+        file.close()
+
+        # Set up the tiles from loaded data
+        self.tiles = []
+        for line in data.split('\n'):
+            row = []
+            for tile_number in line:
+                row.append(int(tile_number))
+            self.tiles.append(row)
+
+        # Keep a list of different kinds of files (grass, sand, water, etc.)
+        self.tile_kinds = tile_kinds
+
+        # How big in pixels are the tiles?
+        self.tile_size = tile_size
+
+    def draw(self, screen):
+        # Go row by row
+        for y, row in enumerate(self.tiles):
+            # Within the current row, go through each tile
+            for x, tile in enumerate(row):
+                location = (x * self.tile_size, y * self.tile_size)
+                screen.blit(tile.image, location)
